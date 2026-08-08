51bdc8cb76b34d7364331379340058f1b8cdb64b
magicon-top <sontrava@gmail.com>
2026-08-08 07:10:37 +0300

    feat: initialize Go module and project structure with required dependencies

--- src/go.mod
+++ src/go.mod
@@ -2,6 +2,4 @@ module go-Splash-dll
 
 go 1.26.5
 
-require (
-	github.com/magicon-top/go-pkg main
-)
+require github.com/magicon-top/go-pkg v0.0.0-20260808034530-38f6bcca3bc2
--- src/main.go
+++ src/main.go
@@ -2,7 +2,7 @@ package main
 import (
 	"fmt"
 	"github.com/magicon-top/go-pkg/dll"
-	)
+)
 
 func main() {	// Теперь мы вызываем функцию через имя пакета: dll.Dll
 	_, err := dll.Dll("asm-Splash-dll.dll", "ShowSplash", 3000)
